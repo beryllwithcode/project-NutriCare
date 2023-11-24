49be41b761138c11fe0533faa9e383fe303660fa
вєя <72957794+bersianturi@users.noreply.github.com>
2023-11-24 22:59:56 +0700

    making component for Community page

--- src/pages/Community.jsx
+++ src/pages/Community.jsx
@@ -1,12 +1,266 @@
-import React from 'react'
-import { Typography } from '@material-tailwind/react'
+import React from "react";
+import { Button, Typography } from "@material-tailwind/react";
+
+const Hero = () => {
+  return (
+    <div className="relative flex bg-gradient-to-b from-white to-light-green-200 justify-center px-10 gap-14 lg:px-24 py-14 text-nutricare-green">
+      <img
+        src="illustration/rafiki.svg"
+        alt="Anatomy"
+        className="h-72 hidden lg:block xl:block"
+      />
+      <div className="max-w-xl">
+        <div>
+          <div>
+            <Typography variant="h1" className="mb-4">
+              Welcome to NutriCare's{" "}
+              <span className="text-nutricare-orange">Community</span>
+            </Typography>
+          </div>
+          <div>
+            <Typography variant="paragraph" className="text-lg">
+              NutriCare's{" "}
+              <span className="text-nutricare-orange">Health Articles</span>{" "}
+              Hub, your go-to resource for a holistic approach to well-being.
+              Immerse yourself in a diverse collection of articles meticulously
+              crafted to empower you on your journey to a{" "}
+              <span className="text-nutricare-orange">healthier lifestyle</span>
+              .
+            </Typography>
+          </div>
+          <div>
+            <div className="mt-4 gap-x-6">
+              <a href="#">
+                <Button className="bg-nutricare-green hover:bg-green-500">
+                  Join Us
+                </Button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Content = () => {
+  return (
+    <div className="relative flex px-10 gap-14 lg:px-24 py-14 text-nutricare-green">
+      <div className="w-screen">
+        <div className="flex justify-between items-center pb-4 border-b-4">
+          <div>
+            <Typography variant="h3">Let's Discuss</Typography>
+            <Typography variant="paragraph">
+              Discuss anything, ask questions, find supports, and connect with
+              others.
+            </Typography>
+          </div>
+          <Button className="bg-nutricare-green h-full">New Discussion</Button>
+        </div>
+        <div className="flex gap-4 my-4">
+          <div>
+            <input
+              class="sr-only peer"
+              type="radio"
+              value="yes"
+              name="sort"
+              id="all"
+            />
+            <label
+              class="flex py-3 px-6 uppercase font-bold text-xs bg-white border border-nutricare-green rounded-lg cursor-pointer focus:outline-none hover:bg-gray-50 peer-checked:bg-nutricare-green peer-checked:text-white"
+              for="all"
+            >
+              All
+            </label>
+          </div>
+          <div>
+            <input
+              class="sr-only peer"
+              type="radio"
+              value="no"
+              name="sort"
+              id="Latest"
+            />
+            <label
+              class="flex py-3 px-6 uppercase font-bold text-xs bg-white border border-nutricare-green rounded-lg cursor-pointer focus:outline-none hover:bg-gray-50 peer-checked:bg-nutricare-green peer-checked:text-white"
+              for="Latest"
+            >
+              Latest
+            </label>
+          </div>
+          <div>
+            <input
+              class="sr-only peer"
+              type="radio"
+              value="no"
+              name="sort"
+              id="Popular"
+            />
+            <label
+              class="flex py-3 px-6 uppercase font-bold text-xs bg-white border border-nutricare-green rounded-lg cursor-pointer focus:outline-none hover:bg-gray-50 peer-checked:bg-nutricare-green peer-checked:text-white"
+              for="Popular"
+            >
+              Popular
+            </label>
+          </div>
+          {/* <Button
+            variant="outlined"
+            className="border-nutricare-green text-nutricare-green hover:bg-nutricare-green focus:bg-nutricare-green focus:text-white hover:text-white"
+          >
+            All
+          </Button>
+          <Button
+            variant="outlined"
+            className="border-nutricare-green text-nutricare-green hover:bg-nutricare-green hover:text-white"
+          >
+            Latest
+          </Button>
+          <Button
+            variant="outlined"
+            className="border-nutricare-green text-nutricare-green hover:bg-nutricare-green hover:text-white"
+          >
+            Popular
+          </Button> */}
+        </div>
+        <div className="flex flex-col gap-4">
+          <div className="p-5 border-2 rounded-lg flex justify-between">
+            <div>
+              <Typography variant="h5">
+                Apa yang akan terjadi ketika kamu diet tidak makan karbo?
+              </Typography>
+              <div className="flex gap-8">
+                <Typography variant="paragraph" className="text-gray-500">
+                  Post by <span className="text-nutricare-orange">Gusnand</span>
+                </Typography>
+                <Typography variant="paragraph" className="text-gray-500">
+                  18 Nov
+                </Typography>
+              </div>
+            </div>
+            <div className="flex gap-4 text-nutricare-greenTuaFade">
+              <div className="flex gap-2 items-center">
+                <img src="/icons/chat-fade.svg" alt="" />
+                <Typography>99</Typography>
+              </div>
+              <div className="flex gap-2 items-center">
+                <img src="/icons/thumb-fade.svg" alt="" />
+                <Typography>99</Typography>
+              </div>
+            </div>
+          </div>
+          <div className="p-5 border-2 rounded-lg flex justify-between">
+            <div>
+              <Typography variant="h5">
+                Apa yang akan terjadi ketika kamu diet tidak makan karbo?
+              </Typography>
+              <div className="flex gap-8">
+                <Typography variant="paragraph" className="text-gray-500">
+                  Post by <span className="text-nutricare-orange">Gusnand</span>
+                </Typography>
+                <Typography variant="paragraph" className="text-gray-500">
+                  18 Nov
+                </Typography>
+              </div>
+            </div>
+            <div className="flex gap-4 text-nutricare-greenTuaFade">
+              <div className="flex gap-2 items-center">
+                <img src="/icons/chat-fade.svg" alt="" />
+                <Typography>99</Typography>
+              </div>
+              <div className="flex gap-2 items-center">
+                <img src="/icons/thumb-fade.svg" alt="" />
+                <Typography>99</Typography>
+              </div>
+            </div>
+          </div>
+          <div className="p-5 border-2 rounded-lg flex justify-between">
+            <div>
+              <Typography variant="h5">
+                Apa yang akan terjadi ketika kamu diet tidak makan karbo?
+              </Typography>
+              <div className="flex gap-8">
+                <Typography variant="paragraph" className="text-gray-500">
+                  Post by <span className="text-nutricare-orange">Gusnand</span>
+                </Typography>
+                <Typography variant="paragraph" className="text-gray-500">
+                  18 Nov
+                </Typography>
+              </div>
+            </div>
+            <div className="flex gap-4 text-nutricare-greenTuaFade">
+              <div className="flex gap-2 items-center">
+                <img src="/icons/chat-fade.svg" alt="" />
+                <Typography>99</Typography>
+              </div>
+              <div className="flex gap-2 items-center">
+                <img src="/icons/thumb-fade.svg" alt="" />
+                <Typography>99</Typography>
+              </div>
+            </div>
+          </div>
+          <div className="p-5 border-2 rounded-lg flex justify-between">
+            <div>
+              <Typography variant="h5">
+                Apa yang akan terjadi ketika kamu diet tidak makan karbo?
+              </Typography>
+              <div className="flex gap-8">
+                <Typography variant="paragraph" className="text-gray-500">
+                  Post by <span className="text-nutricare-orange">Gusnand</span>
+                </Typography>
+                <Typography variant="paragraph" className="text-gray-500">
+                  18 Nov
+                </Typography>
+              </div>
+            </div>
+            <div className="flex gap-4 text-nutricare-greenTuaFade">
+              <div className="flex gap-2 items-center">
+                <img src="/icons/chat-fade.svg" alt="" />
+                <Typography>99</Typography>
+              </div>
+              <div className="flex gap-2 items-center">
+                <img src="/icons/thumb-fade.svg" alt="" />
+                <Typography>99</Typography>
+              </div>
+            </div>
+          </div>
+          <div className="p-5 border-2 rounded-lg flex justify-between">
+            <div>
+              <Typography variant="h5">
+                Apa yang akan terjadi ketika kamu diet tidak makan karbo?
+              </Typography>
+              <div className="flex gap-8">
+                <Typography variant="paragraph" className="text-gray-500">
+                  Post by <span className="text-nutricare-orange">Gusnand</span>
+                </Typography>
+                <Typography variant="paragraph" className="text-gray-500">
+                  18 Nov
+                </Typography>
+              </div>
+            </div>
+            <div className="flex gap-4 text-nutricare-greenTuaFade">
+              <div className="flex gap-2 items-center">
+                <img src="/icons/chat-fade.svg" alt="" />
+                <Typography>99</Typography>
+              </div>
+              <div className="flex gap-2 items-center">
+                <img src="/icons/thumb-fade.svg" alt="" />
+                <Typography>99</Typography>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 function Community() {
   return (
-    <Typography variant='h2' className='text-center'>
-      Community
-    </Typography>
-  )
+    <>
+      <Hero />
+      <Content />
+    </>
+  );
 }
 
-export default Community
+export default Community;
