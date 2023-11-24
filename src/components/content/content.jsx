ab2d7398d84edf5a22cc1464ddc4eadcd262456b
вєя <72957794+bersianturi@users.noreply.github.com>
2023-11-24 15:27:19 +0700

    make description component separately

--- src/components/content/content.jsx
+++ src/components/content/content.jsx
@@ -11,64 +11,65 @@ import {
 import React, { useRef } from "react";
 import ScrollCarousel from "scroll-carousel-react";
 import { motion } from "framer-motion";
+import Description from "./description";
 
-const Description = () => {
-  const scrollRef = useRef(null);
-  return (
-    <div className="relative flex gap-14 justify-center px-10 lg:px-24 py-14 text-nutricare-green">
-      <motion.img
-        initial={{ opacity: 0, x: "-100px" }}
-        whileInView={{ opacity: 1, x: 0 }}
-        viewport={{ root: scrollRef }}
-        transition={{
-          delay: 0.25,
-          duration: 0.75,
-          type: "spring",
-          stiffness: 150,
-        }}
-        src="illustration/pana.png"
-        alt="Anatomy"
-        className="h-72 hidden lg:block xl:block"
-      ></motion.img>
-      <motion.div
-        initial={{ opacity: 0, x: "100px" }}
-        whileInView={{ opacity: 1, x: 0 }}
-        viewport={{ root: scrollRef }}
-        transition={{
-          delay: 0.25,
-          duration: 0.75,
-          type: "spring",
-          stiffness: 150,
-        }}
-        className="max-w-2xl"
-      >
-        <Typography variant="h1" className="mb-4">
-          What is <span className="text-nutricare-orange">Body-Mass Index</span>
-          <br />
-          (BMI)?
-        </Typography>
-        <Typography variant="paragraph" className="text-lg">
-          <span className="text-nutricare-orange">NutriCare</span> is your
-          one-stop destination for achieving a healthier lifestyle through
-          proper nutrition. Our platform is designed to empower you with the
-          knowledge and tools needed to make informed decisions about your diet
-          and overall well-being. Whether you're looking to manage your weight,
-          improve your eating habits, or simply understand the nutritional
-          content of your meals,{" "}
-          <span className="text-nutricare-orange">NutriCare</span> has you
-          covered.
-        </Typography>
-        <div className="mt-4 gap-x-6">
-          <a href="#">
-            <Button className="bg-nutricare-green hover:bg-green-500">
-              Read More
-            </Button>
-          </a>
-        </div>
-      </motion.div>
-    </div>
-  );
-};
+// const Description = () => {
+//   const scrollRef = useRef(null);
+//   return (
+//     <div className="relative flex gap-14 justify-center px-10 lg:px-24 py-14 text-nutricare-green">
+//       <motion.img
+//         initial={{ opacity: 0, x: "-100px" }}
+//         whileInView={{ opacity: 1, x: 0 }}
+//         viewport={{ root: scrollRef }}
+//         transition={{
+//           delay: 0.25,
+//           duration: 0.75,
+//           type: "spring",
+//           stiffness: 150,
+//         }}
+//         src="illustration/pana.png"
+//         alt="Anatomy"
+//         className="h-72 hidden lg:block xl:block"
+//       ></motion.img>
+//       <motion.div
+//         initial={{ opacity: 0, x: "100px" }}
+//         whileInView={{ opacity: 1, x: 0 }}
+//         viewport={{ root: scrollRef }}
+//         transition={{
+//           delay: 0.25,
+//           duration: 0.75,
+//           type: "spring",
+//           stiffness: 150,
+//         }}
+//         className="max-w-2xl"
+//       >
+//         <Typography variant="h1" className="mb-4">
+//           What is <span className="text-nutricare-orange">Body-Mass Index</span>
+//           <br />
+//           (BMI)?
+//         </Typography>
+//         <Typography variant="paragraph" className="text-lg">
+//           <span className="text-nutricare-orange">NutriCare</span> is your
+//           one-stop destination for achieving a healthier lifestyle through
+//           proper nutrition. Our platform is designed to empower you with the
+//           knowledge and tools needed to make informed decisions about your diet
+//           and overall well-being. Whether you're looking to manage your weight,
+//           improve your eating habits, or simply understand the nutritional
+//           content of your meals,{" "}
+//           <span className="text-nutricare-orange">NutriCare</span> has you
+//           covered.
+//         </Typography>
+//         <div className="mt-4 gap-x-6">
+//           <a href="#">
+//             <Button className="bg-nutricare-green hover:bg-green-500">
+//               Read More
+//             </Button>
+//           </a>
+//         </div>
+//       </motion.div>
+//     </div>
+//   );
+// };
 
 const Services = () => {
   const scrollRef = useRef(null);
