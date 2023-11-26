37998a512ca447c147ae7a560f4e013883067980
вєя <72957794+bersianturi@users.noreply.github.com>
2023-11-26 15:43:10 +0700

    fix responsive

--- src/pages/Community.jsx
+++ src/pages/Community.jsx
@@ -48,14 +48,16 @@ const Content = () => {
     <div className="relative flex px-10 gap-14 lg:px-24 py-14 text-nutricare-green">
       <div className="w-screen">
         <div className="flex justify-between items-center pb-4 border-b-4">
-          <div>
+          <div className="w-64 lg:w-full">
             <Typography variant="h3">Let's Discuss</Typography>
             <Typography variant="paragraph">
               Discuss anything, ask questions, find supports, and connect with
               others.
             </Typography>
           </div>
-          <Button className="bg-nutricare-green h-full">New Discussion</Button>
+          <Button className="bg-nutricare-green w-32 lg:w-100 h-full">
+            New Discussion
+          </Button>
         </div>
         <div className="flex gap-4 my-4">
           <div>
@@ -65,6 +67,7 @@ const Content = () => {
               value="yes"
               name="sort"
               id="all"
+              defaultChecked
             />
             <label
               class="flex py-3 px-6 uppercase font-bold text-xs bg-white border border-nutricare-green rounded-lg cursor-pointer focus:outline-none hover:bg-gray-50 peer-checked:bg-nutricare-green peer-checked:text-white"
@@ -103,28 +106,10 @@ const Content = () => {
               Popular
             </label>
           </div>
-          {/* <Button
-            variant="outlined"
-            className="border-nutricare-green text-nutricare-green hover:bg-nutricare-green focus:bg-nutricare-green focus:text-white hover:text-white"
-          >
-            All
-          </Button>
-          <Button
-            variant="outlined"
-            className="border-nutricare-green text-nutricare-green hover:bg-nutricare-green hover:text-white"
-          >
-            Latest
-          </Button>
-          <Button
-            variant="outlined"
-            className="border-nutricare-green text-nutricare-green hover:bg-nutricare-green hover:text-white"
-          >
-            Popular
-          </Button> */}
         </div>
         <div className="flex flex-col gap-4">
-          <div className="p-5 border-2 rounded-lg flex justify-between">
-            <div>
+          <div className="p-5 border-2 rounded-lg flex items-center justify-between">
+            <div className="w-72 lg:w-full">
               <Typography variant="h5">
                 Apa yang akan terjadi ketika kamu diet tidak makan karbo?
               </Typography>
@@ -137,19 +122,19 @@ const Content = () => {
                 </Typography>
               </div>
             </div>
-            <div className="flex gap-4 text-nutricare-greenTuaFade">
+            <div className="flex gap-2 lg:gap-6 flex-col lg:flex-row text-nutricare-greenTuaFade">
               <div className="flex gap-2 items-center">
-                <img src="/icons/chat-fade.svg" alt="" />
+                <img src="/icons/chat-fade.svg" alt="" className="w-5" />
                 <Typography>99</Typography>
               </div>
               <div className="flex gap-2 items-center">
-                <img src="/icons/thumb-fade.svg" alt="" />
+                <img src="/icons/thumb-fade.svg" alt="" className="w-5" />
                 <Typography>99</Typography>
               </div>
             </div>
           </div>
-          <div className="p-5 border-2 rounded-lg flex justify-between">
-            <div>
+          <div className="p-5 border-2 rounded-lg flex items-center justify-between">
+            <div className="w-72 lg:w-full">
               <Typography variant="h5">
                 Apa yang akan terjadi ketika kamu diet tidak makan karbo?
               </Typography>
@@ -162,19 +147,19 @@ const Content = () => {
                 </Typography>
               </div>
             </div>
-            <div className="flex gap-4 text-nutricare-greenTuaFade">
+            <div className="flex gap-2 lg:gap-6 flex-col lg:flex-row text-nutricare-greenTuaFade">
               <div className="flex gap-2 items-center">
-                <img src="/icons/chat-fade.svg" alt="" />
+                <img src="/icons/chat-fade.svg" alt="" className="w-5" />
                 <Typography>99</Typography>
               </div>
               <div className="flex gap-2 items-center">
-                <img src="/icons/thumb-fade.svg" alt="" />
+                <img src="/icons/thumb-fade.svg" alt="" className="w-5" />
                 <Typography>99</Typography>
               </div>
             </div>
           </div>
-          <div className="p-5 border-2 rounded-lg flex justify-between">
-            <div>
+          <div className="p-5 border-2 rounded-lg flex items-center justify-between">
+            <div className="w-72 lg:w-full">
               <Typography variant="h5">
                 Apa yang akan terjadi ketika kamu diet tidak makan karbo?
               </Typography>
@@ -187,19 +172,19 @@ const Content = () => {
                 </Typography>
               </div>
             </div>
-            <div className="flex gap-4 text-nutricare-greenTuaFade">
+            <div className="flex gap-2 lg:gap-6 flex-col lg:flex-row text-nutricare-greenTuaFade">
               <div className="flex gap-2 items-center">
-                <img src="/icons/chat-fade.svg" alt="" />
+                <img src="/icons/chat-fade.svg" alt="" className="w-5" />
                 <Typography>99</Typography>
               </div>
               <div className="flex gap-2 items-center">
-                <img src="/icons/thumb-fade.svg" alt="" />
+                <img src="/icons/thumb-fade.svg" alt="" className="w-5" />
                 <Typography>99</Typography>
               </div>
             </div>
           </div>
-          <div className="p-5 border-2 rounded-lg flex justify-between">
-            <div>
+          <div className="p-5 border-2 rounded-lg flex items-center justify-between">
+            <div className="w-72 lg:w-full">
               <Typography variant="h5">
                 Apa yang akan terjadi ketika kamu diet tidak makan karbo?
               </Typography>
@@ -212,19 +197,19 @@ const Content = () => {
                 </Typography>
               </div>
             </div>
-            <div className="flex gap-4 text-nutricare-greenTuaFade">
+            <div className="flex gap-2 lg:gap-6 flex-col lg:flex-row text-nutricare-greenTuaFade">
               <div className="flex gap-2 items-center">
-                <img src="/icons/chat-fade.svg" alt="" />
+                <img src="/icons/chat-fade.svg" alt="" className="w-5" />
                 <Typography>99</Typography>
               </div>
               <div className="flex gap-2 items-center">
-                <img src="/icons/thumb-fade.svg" alt="" />
+                <img src="/icons/thumb-fade.svg" alt="" className="w-5" />
                 <Typography>99</Typography>
               </div>
             </div>
           </div>
-          <div className="p-5 border-2 rounded-lg flex justify-between">
-            <div>
+          <div className="p-5 border-2 rounded-lg flex items-center justify-between">
+            <div className="w-72 lg:w-full">
               <Typography variant="h5">
                 Apa yang akan terjadi ketika kamu diet tidak makan karbo?
               </Typography>
@@ -237,13 +222,38 @@ const Content = () => {
                 </Typography>
               </div>
             </div>
-            <div className="flex gap-4 text-nutricare-greenTuaFade">
+            <div className="flex gap-2 lg:gap-6 flex-col lg:flex-row text-nutricare-greenTuaFade">
               <div className="flex gap-2 items-center">
-                <img src="/icons/chat-fade.svg" alt="" />
+                <img src="/icons/chat-fade.svg" alt="" className="w-5" />
                 <Typography>99</Typography>
               </div>
               <div className="flex gap-2 items-center">
-                <img src="/icons/thumb-fade.svg" alt="" />
+                <img src="/icons/thumb-fade.svg" alt="" className="w-5" />
+                <Typography>99</Typography>
+              </div>
+            </div>
+          </div>
+          <div className="p-5 border-2 rounded-lg flex items-center justify-between">
+            <div className="w-72 lg:w-full">
+              <Typography variant="h5">
+                Apa yang akan terjadi ketika kamu diet tidak makan karbo?
+              </Typography>
+              <div className="flex gap-8">
+                <Typography variant="paragraph" className="text-gray-500">
+                  Post by <span className="text-nutricare-orange">Gusnand</span>
+                </Typography>
+                <Typography variant="paragraph" className="text-gray-500">
+                  18 Nov
+                </Typography>
+              </div>
+            </div>
+            <div className="flex gap-2 lg:gap-6 flex-col lg:flex-row text-nutricare-greenTuaFade">
+              <div className="flex gap-2 items-center">
+                <img src="/icons/chat-fade.svg" alt="" className="w-5" />
+                <Typography>99</Typography>
+              </div>
+              <div className="flex gap-2 items-center">
+                <img src="/icons/thumb-fade.svg" alt="" className="w-5" />
                 <Typography>99</Typography>
               </div>
             </div>
