12f92de3bb7af336a7b6ccbb8d2d5c2b562964e6
вєя <72957794+bersianturi@users.noreply.github.com>
2023-11-29 19:44:42 +0700

    add scroll to top component

--- src/layout/Index.jsx
+++ src/layout/Index.jsx
@@ -1,10 +1,12 @@
 import Footer from "../components/footer/footer";
 import NavbarDefault from "../components/navbar/navbar";
+import { ScrollTop } from "../components/scrolltotop";
 
 function Layout({ children }) {
   return (
     <>
       <NavbarDefault />
+      <ScrollTop />
       <main>{children}</main>
       <Footer />
     </>
