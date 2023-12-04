b8dd6672f65870090f4fd94661274d50723f7fce
вєя <72957794+bersianturi@users.noreply.github.com>
2023-12-04 12:54:38 +0700

    add bmi calculate feature

--- src/pages/BMI_Test.jsx
+++ src/pages/BMI_Test.jsx
@@ -8,7 +8,7 @@ import {
   Button,
   Chip,
 } from "@material-tailwind/react";
-import React from "react";
+import React, { useState } from "react";
 import Description from "../components/content/description";
 
 function Guidance() {
@@ -91,127 +91,245 @@ function Guidance() {
 }
 
 function Test() {
+  const [genderValue, setGenderValue] = useState("");
+  const [ageValue, setAgeValue] = useState("");
+  const [heightValue, setHeightValue] = useState("");
+  const [weightValue, setWeightValue] = useState("");
+  const [bmiMessage, setBmiMessage] = useState("");
+  const [calorieNeeds, setCalorieNeeds] = useState("");
+  const [proteinNeeds, setProteinsNeeds] = useState("");
+  const [fatNeeds, setFatNeeds] = useState("");
+  const [bmiIllustration, setBmiIllustration] = useState("");
+  const [bmiColor, setBmiColor] = useState("");
+
+  const calculateBmi = () => {
+    if (heightValue && weightValue) {
+      const heightInMeters = heightValue / 100;
+      const bmi = (weightValue / (heightInMeters * heightInMeters)).toFixed(2);
+
+      let message = "";
+      let illustration = "";
+      let color = "";
+
+      if (bmi < 18.5) {
+        message = "Underweight";
+        illustration = "worried.svg";
+        color = "bg-nutricare-orange";
+      } else if (bmi >= 18.5 && bmi < 25) {
+        message = "Normal";
+        illustration = "healthy.svg";
+        color = "bg-nutricare-green";
+      } else if (bmi >= 25 && bmi < 30) {
+        message = "Overweight";
+        illustration = "worried.svg";
+        color = "bg-nutricare-orange";
+      } else {
+        message = "Obese";
+        illustration = "worried.svg";
+        color = "bg-nutricare-orange";
+      }
+
+      let calorie = "";
+      let protein = "";
+      let fat = "";
+
+      if (genderValue === "Male") {
+        calorie = (
+          13.397 * weightValue +
+          4.799 * heightValue -
+          5.677 * ageValue +
+          88.362
+        ).toFixed();
+        protein = (0.8 * weightValue).toFixed();
+        fat = (0.25 * calorie).toFixed();
+      } else if (genderValue === "Female") {
+        calorie =
+          9.247 * weightValue + 3.098 * heightValue - 4.33 * ageValue + 447.593;
+        protein = 0.8 * weightValue;
+        fat = 0.25 * calorie;
+      } else {
+        setCalorieNeeds("");
+        setProteinsNeeds("");
+        setFatNeeds("");
+      }
+
+      setBmiMessage(message);
+      setBmiIllustration(illustration);
+      setBmiColor(color);
+      setCalorieNeeds(calorie + "kcal/day");
+      setProteinsNeeds(protein + "g/day");
+      setFatNeeds(fat + "g/day");
+    } else {
+      setBmiMessage("");
+      setBmiIllustration("pana.png");
+      setBmiColor("greenTuaFade");
+      setCalorieNeeds("");
+      setProteinsNeeds("");
+      setFatNeeds("");
+    }
+  };
+
   return (
-    <div className="relative flex flex-col justify-center text-center my-16 py-8 bg-nutricare-greenMudaFade text-nutricare-green">
-      <div className="mb-4">
-        <Typography variant="h1" className="mb-2">
-          Calculate BMI <span className="text-nutricare-orange">Here</span>
-        </Typography>
-        <Typography variant="paragraph" className="text-lg">
-          Make sure you input the data correctly
-        </Typography>
+    <>
+      <div className="relative flex flex-col justify-center text-center my-16 py-8 bg-nutricare-greenMudaFade text-nutricare-green">
+        <div className="mb-4">
+          <Typography variant="h1" className="mb-2">
+            Calculate BMI <span className="text-nutricare-orange">Here</span>
+          </Typography>
+          <Typography variant="paragraph" className="text-lg">
+            Make sure you input the data correctly
+          </Typography>
+        </div>
+        <form className="flex flex-col justify-center">
+          <div
+            className="flex justify-center gap-10"
+            onChange={(e) => setGenderValue(e.target.value)}
+          >
+            <Radio
+              name="gender"
+              color="green"
+              ripple={true}
+              label="Male"
+              value="Male"
+            />
+            <Radio
+              name="gender"
+              value="Female"
+              color="green"
+              ripple={true}
+              label="Female"
+            />
+          </div>
+          <div className="flex flex-col w-72 mx-auto lg:flex-row justify-center gap-6 my-4">
+            <div className="flex flex-col gap-1">
+              <Typography variant="paragraph" className="text-left text-medium">
+                Age (years)
+              </Typography>
+              <Input
+                placeholder="ex: 20"
+                className="!border-nutricare-green focus:!border-nutricare-green"
+                labelProps={{
+                  className: "before:content-none after:content-none",
+                }}
+                onChange={(e) => setAgeValue(e.target.value)}
+              ></Input>
+            </div>
+            <div className="flex flex-col gap-1">
+              <Typography variant="paragraph" className="text-left text-medium">
+                Height (cm)
+              </Typography>
+              <Input
+                placeholder="ex: 170"
+                className="!border-nutricare-green focus:!border-nutricare-green"
+                labelProps={{
+                  className: "before:content-none after:content-none",
+                }}
+                value={heightValue}
+                onChange={(e) => setHeightValue(e.target.value)}
+              ></Input>
+            </div>
+            <div className="flex flex-col gap-1">
+              <Typography variant="paragraph" className="text-left text-medium">
+                Weight (kg)
+              </Typography>
+              <Input
+                placeholder="ex: 60"
+                className="!border-nutricare-green focus:!border-nutricare-green"
+                labelProps={{
+                  className: "before:content-none after:content-none",
+                }}
+                value={weightValue}
+                onChange={(e) => setWeightValue(e.target.value)}
+              ></Input>
+            </div>
+          </div>
+          <div>
+            <Button
+              onClick={calculateBmi}
+              className="bg-nutricare-green hover:bg-green-500 mt-2"
+            >
+              Calculate!
+            </Button>
+          </div>
+        </form>
       </div>
-      <form action="" className="flex flex-col justify-center">
-        <div className="flex justify-center gap-10">
-          <Radio
-            name="gender"
-            color="green"
-            ripple={true}
-            label="Male"
-            defaultChecked
+      <div className="relative mx-4 lg:mx-auto max-w-6xl rounded-3xl flex flex-col justify-center text-center my-32 py-8 text-nutricare-green border-2 border-nutricare-green">
+        <div className="mb-4">
+          <Typography variant="h1" className="mb-2">
+            The <span className="text-nutricare-orange">Result</span>
+          </Typography>
+        </div>
+        <div className="flex justify-center gap-32 mr-0 lg:mr-32 ">
+          <img
+            src={
+              bmiIllustration
+                ? "illustration/" + bmiIllustration
+                : "illustration/pana.png"
+            }
+            alt="Anatomy"
+            className="hidden lg:block"
           />
-          <Radio name="gender" color="green" ripple={true} label="Female" />
-        </div>
-        <div className="flex flex-col w-72 mx-auto lg:flex-row justify-center gap-6 my-4">
-          <div className="flex flex-col gap-1">
-            <Typography variant="paragraph" className="text-left text-medium">
-              Age (years)
-            </Typography>
-            <Input
-              placeholder="ex: 20"
-              className="!border-nutricare-green focus:!border-nutricare-green"
-              labelProps={{
-                className: "before:content-none after:content-none",
-              }}
-            ></Input>
+          <div className="flex flex-col gap-6">
+            <div className="flex flex-col justify-start gap-2">
+              <Typography>Your BMI Status</Typography>
+              <Chip
+                size="lg"
+                className={
+                  bmiColor
+                    ? "text-xs " + bmiColor
+                    : "text-xs bg-nutricare-greenTuaFade"
+                }
+                value={bmiMessage ? bmiMessage : "UNKNOWN"}
+              />
+            </div>
+            <div className="flex flex-col justify-start gap-2">
+              <Typography>Calorie Needs</Typography>
+              <Chip
+                size="lg"
+                className={
+                  bmiMessage
+                    ? "bg-nutricare-green text-xs"
+                    : "bg-nutricare-greenTuaFade text-xs"
+                }
+                value={calorieNeeds ? calorieNeeds : "UNKNOWN"}
+              />
+            </div>
+            <div className="flex flex-col justify-start gap-2">
+              <Typography>Protein Needs</Typography>
+              <Chip
+                size="lg"
+                className={
+                  bmiMessage
+                    ? "bg-nutricare-green text-xs"
+                    : "bg-nutricare-greenTuaFade text-xs"
+                }
+                value={proteinNeeds ? proteinNeeds : "UNKNOWN"}
+              />
+            </div>
+            <div className="flex flex-col justify-start gap-2">
+              <Typography>Fat Needs</Typography>
+              <Chip
+                size="lg"
+                className={
+                  bmiMessage
+                    ? "bg-nutricare-green text-xs"
+                    : "bg-nutricare-greenTuaFade text-xs"
+                }
+                value={fatNeeds ? fatNeeds : "UNKNOWN"}
+              />
+            </div>
           </div>
-          <div className="flex flex-col gap-1">
-            <Typography variant="paragraph" className="text-left text-medium">
-              Height (cm)
-            </Typography>
-            <Input
-              placeholder="ex: 170"
-              className="!border-nutricare-green focus:!border-nutricare-green"
-              labelProps={{
-                className: "before:content-none after:content-none",
-              }}
-            ></Input>
-          </div>
-          <div className="flex flex-col gap-1">
-            <Typography variant="paragraph" className="text-left text-medium">
-              Weight (kg)
-            </Typography>
-            <Input
-              placeholder="ex: 60"
-              className="!border-nutricare-green focus:!border-nutricare-green"
-              labelProps={{
-                className: "before:content-none after:content-none",
-              }}
-            ></Input>
-          </div>
         </div>
-        <div>
-          <Button className="bg-nutricare-green hover:bg-green-500 mt-2">
-            Calculate!
-          </Button>
-        </div>
-      </form>
-    </div>
+      </div>
+    </>
   );
 }
 
-function Result() {
-  return (
-    <div className="relative mx-4 lg:mx-auto max-w-6xl rounded-3xl flex flex-col justify-center text-center my-32 py-8 text-nutricare-green border-2 border-nutricare-green">
-      <div className="mb-4">
-        <Typography variant="h1" className="mb-2">
-          The <span className="text-nutricare-orange">Result</span>
-        </Typography>
-      </div>
-      <div className="flex justify-center gap-32 mr-0 lg:mr-32 ">
-        <img
-          src="illustration/pana.png"
-          alt="Anatomy"
-          className="hidden lg:block"
-        />
-        <div className="flex flex-col gap-6">
-          <div className="flex flex-col justify-start gap-2">
-            <Typography>Your BMI Status</Typography>
-            <Chip
-              size="lg"
-              className="bg-nutricare-greenTuaFade text-xs"
-              value="Unknown"
-            />
-          </div>
-          <div className="flex flex-col justify-start gap-2">
-            <Typography>Calorie Needs</Typography>
-            <Chip
-              size="lg"
-              className="bg-nutricare-greenTuaFade text-xs"
-              value="Unknown"
-            />
-          </div>
-          <div className="flex flex-col justify-start gap-2">
-            <Typography>Protein Needs</Typography>
-            <Chip
-              size="lg"
-              className="bg-nutricare-greenTuaFade text-xs"
-              value="Unknown"
-            />
-          </div>
-          <div className="flex flex-col justify-start gap-2">
-            <Typography>Fat Needs</Typography>
-            <Chip
-              size="lg"
-              className="bg-nutricare-greenTuaFade text-xs"
-              value="Unknown"
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
+// function Result() {
+//   return (
+
+//   );
+// }
 
 function BMITest() {
   return (
@@ -219,7 +337,7 @@ function BMITest() {
       <Description />
       <Guidance />
       <Test />
-      <Result />
+      {/* <Result /> */}
     </>
   );
 }
