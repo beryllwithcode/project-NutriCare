dcc50496aeec0c1a83a65341913a43ac00132371
Gusnand <gusnanda1170@gmail.com>
2023-12-11 10:59:55 +0800

    adjustment in navbar dom and e2e testing

--- e2e/community.spec.ts
+++ e2e/community.spec.ts
@@ -75,10 +75,10 @@ test("Adding comment to the discussion", async ({ page }) => {
       .getByRole("button")
   ).toBeVisible();
   await page.getByRole("link", { name: "Community" }).first().click();
-  await expect(page.locator("#discussion")).toContainText(
-    "End to End Testing for Adding Discussion"
-  );
-  await expect(page.locator("#discussion")).toContainText("Post by user");
+  await page.reload({ timeout: 6000 });
+  await expect(
+    page.getByRole("link", { name: "End to End Testing for Adding" })
+  ).toBeVisible();
   await page
     .getByRole("link", { name: "End to End Testing for Adding" })
     .click();
@@ -86,17 +86,6 @@ test("Adding comment to the discussion", async ({ page }) => {
   await page.getByPlaceholder(" ").click();
   await page.getByPlaceholder(" ").fill("Adding Comment");
   await page.getByRole("button", { name: "Post Comments" }).click();
-  page.once("dialog", (dialog) => {
-    console.log(`Dialog message: ${dialog.message()}`);
-    dialog.dismiss().catch(() => {});
-  });
-  await expect(page.getByRole("heading", { name: "Comments" })).toBeVisible();
-  await expect(
-    page
-      .locator("p")
-      .filter({ hasText: /^user$/ })
-      .locator("span")
-  ).toBeVisible();
   await expect(page.getByText("Adding Comment")).toBeVisible();
 });
 
@@ -118,27 +107,20 @@ test("Deleting discussion created", async ({ page }) => {
       .getByRole("button")
   ).toBeVisible();
   await page.getByRole("link", { name: "Community" }).first().click();
+  await page.reload({ timeout: 6000 });
   await expect(
     page.getByRole("link", { name: "End to End Testing for Adding" })
   ).toBeVisible();
-  await expect(page.getByText("Post by user")).toBeVisible();
   await page
     .getByRole("link", { name: "End to End Testing for Adding" })
     .click();
-  await expect(page.locator("#discussion")).toContainText(
-    "End to End Testing for Adding Discussion"
-  );
-  await expect(page.locator("#discussion")).toContainText(
-    "Description for adding discussion"
-  );
-  await expect(page.locator("#discussion")).toContainText("by user");
   await expect(
     page.getByRole("button", { name: "Delete Discussion" })
   ).toBeVisible();
   await page.getByRole("button", { name: "Delete Discussion" }).click();
-  const dialog = await page.waitForEvent("dialog");
-  expect(dialog.message()).toBe(
+  await expect(page.getByRole("dialog")).toContainText(
     "Are you sure you want to delete this discussion?"
   );
-  await dialog.accept();
+  await expect(page.getByRole("button", { name: "Delete" })).toBeVisible();
+  await page.getByRole("button", { name: "Delete" }).click();
 });
